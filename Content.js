7c324225a42d9a37e1129a46d049d0aa4e64e61c
Aurelia Mika <35524294+Aurelia-Mika@users.noreply.github.com>
2025-11-06 20:54:03 +0100

    order optimization
    change the order

--- Content.js
+++ Content.js
@@ -2,7 +2,20 @@
  if(web.includes("lingos.pl/students/checkAnswer")){
     document.querySelector("a#next").click();
  }
- if(web.includes("lingos.pl/students/wordsList")){
+ if(web.includes("lingos.pl/students/learning")){
+    let local = JSON.parse(window.localStorage.getItem('werb'));
+    let werb = document.querySelector('h3.mb-3.h3').innerHTML;
+    werb = werb.replaceAll("\n","");
+    werb = werb.replaceAll(" ","");
+    if(local[werb]!= undefined){
+        document.querySelector('input#answer.form-control').value = local[werb];
+        document.querySelector("button[type=submit]").click();
+    }
+    else{
+        window.location.replace("https://lingos.pl/students/group");
+    }
+ }
+if(web.includes("lingos.pl/students/wordsList")){
     let gui = document.querySelector('.bg-white.shrinkableDiv');
     gui.innerHTML += '<button class="btn px-2 py-1 btn-soft-primary" id="copy"><span class="d-none d-sm-inline"> Kopiuj</span></button>';
     document.querySelector("#copy").addEventListener('click', copy());
@@ -18,18 +31,5 @@
         });
         window.localStorage.setItem('werb', JSON.stringify(werb));
         let local = JSON.parse(window.localStorage.getItem('werb'));
-    }
- }
- if(web.includes("lingos.pl/students/learning")){
-    let local = JSON.parse(window.localStorage.getItem('werb'));
-    let werb = document.querySelector('h3.mb-3.h3').innerHTML;
-    werb = werb.replaceAll("\n","");
-    werb = werb.replaceAll(" ","");
-    if(local[werb]!= undefined){
-        document.querySelector('input#answer.form-control').value = local[werb];
-        document.querySelector("button[type=submit]").click();
-    }
-    else{
-        window.location.replace("https://lingos.pl/students/group");
     }
  }
